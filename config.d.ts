b5064895785ebf3d77feb651e62ad9422987ab36
b-kelly <b-kelly@users.noreply.github.com>
2021-10-01 23:59:50 +0000

    Deploying to gh-pages from @ b-kelly/ludum-dare-49@13ddf9744cbd5a82b23e1a5668cee5aad6f5e98d 🚀

---
+++ config.d.ts
@@ -0,0 +1,2 @@
+export declare const TILE_WIDTH = 32;
+export declare const gameConfig: Phaser.Types.Core.GameConfig;
---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import "./index.css";
+import * as Phaser from "phaser";
+export declare const game: Phaser.Game;
