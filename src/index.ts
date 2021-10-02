8376eb0a6eb985c3e008f480b20e47ada60d0fd9
Ben Kelly <b-kelly@users.noreply.github.com>
2021-10-01 23:04:12 -0400

    that art was bad, even for me...

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import "./index.css";
 import * as Phaser from "phaser";
 import { gameConfig } from "./config";
-import { Cave } from "./models/Cave";
-import { displayMap } from "./models/Chrome";
 
 export const game = new Phaser.Game(gameConfig);
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 global.instance = game;
-
-// TODO DEBUG
-const cave = new Cave(512, 512);
-displayMap(cave);
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,13 +1,17 @@
 import { TILE_WIDTH } from "../config";
+import { Cave } from "../models/Cave";
+import { displayMap } from "../models/Chrome";
 
-enum TileType {
-    Ground,
-    Robot,
-    Resource,
-    Wall,
-}
+// enum TileType {
+//     Ground,
+//     Robot,
+//     Resource,
+//     Wall,
+// }
 
 export class GameScene extends Phaser.Scene {
+    private cave: Cave;
+
     constructor() {
         super({ key: "Game" });
     }
@@ -24,28 +28,40 @@ export class GameScene extends Phaser.Scene {
     }
 
     preload(): void {
-        this.load.image(TileType[TileType.Ground], "assets/ground.png");
-        this.load.image(TileType[TileType.Robot], "assets/robot.png");
-        this.load.image(TileType[TileType.Resource], "assets/resource.png");
-        this.load.spritesheet(TileType[TileType.Wall], "assets/walls.png", {
-            frameWidth: TILE_WIDTH,
-            frameHeight: TILE_WIDTH,
-        });
+        // this.load.image(TileType[TileType.Ground], "assets/ground.png");
+        // this.load.image(TileType[TileType.Robot], "assets/robot.png");
+        // this.load.image(TileType[TileType.Resource], "assets/resource.png");
+        // this.load.spritesheet(TileType[TileType.Wall], "assets/walls.png", {
+        //     frameWidth: TILE_WIDTH,
+        //     frameHeight: TILE_WIDTH,
+        // });
+
+        // TODO yeah, this is creating a cave that is roughly ((width * height) * TILE_WIDTH^2) in size...
+        this.cave = new Cave(this.width, this.height);
+        displayMap(this.cave);
     }
 
     create(): void {
         this.physics.world.setBounds(0, 0, this.width, this.height);
+        // draw the ground layer
         this.add
-            .tileSprite(
-                0,
-                0,
-                this.width,
-                this.height,
-                TileType[TileType.Ground]
-            )
+            .rectangle(0, 0, this.width, this.height, 0x695958)
             .setOrigin(0);
 
-        this.add.sprite(0, 0, TileType[TileType.Robot]);
+        // draw the robot
+        this.add.rectangle(0, 0, TILE_WIDTH, TILE_WIDTH, 0x006d77).setOrigin(0);
+
+        // create the tilemap
+        const map = this.make.tilemap({
+            data: this.cave.map,
+            tileWidth: TILE_WIDTH,
+            tileHeight: TILE_WIDTH,
+        });
+        const tiles = map.addTilesetImage("walls", "walls");
+        const layer = map.createLayer(0, tiles, 0, 0);
+
+        // TODO resource 140d4f
+        // TODO wall/filled 96ADC8
     }
 
     update(): void {
